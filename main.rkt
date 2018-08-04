2368c694e67e7a21990149a64b902536a5ff536f
Jesse Alama <jesse@lisp.sh>
2018-08-04 14:11:35 +0200

    Settle on the library interface

--- main.rkt
+++ main.rkt
@@ -1,5 +1,14 @@
 #lang br/quicklang
 
+(provide port->ejsexpr
+         port->ejsexprs
+         string->ejsexpr
+         ejsexpr->string
+         ejsexpr->bytes)
+
+(require (file "parse.rkt")
+         (file "render.rkt"))
+
 (module reader br
   (require (file "reader.rkt"))
   (provide read-syntax))
